ea2b3ffbbf1949195defd5a11069c18a50780c8f
unknown <helenvcolon@gmail.com>
2023-10-10 23:24:25 -0400

    Defined variables and initial prompt.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Assignment Code
+/* // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -12,3 +12,19 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+*/
+
+// Variables for Password
+
+var lowcasChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercasChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numericChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"];
+
+// Prompt user to choose if they want lower case, upper case, numeric, and/or special characters in their password.
+
+var charLength = prompt (
+  "How many characters do you want in your password? Choose a length (number) between 8 to 128.)"
+);
+
+
