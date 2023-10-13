777f81b3e7b22d2e3b0d1f3fc1a1d2333681b88f
unknown <helenvcolon@gmail.com>
2023-10-12 23:14:42 -0400

    Added all my if conditional statements and prompts.

--- script.js
+++ script.js
@@ -17,13 +17,45 @@ function writePassword() {
 
   var charLength = parseInt(prompt ("Please enter a number between 8 to 128 for your desired password length."));
 
-  If (charLength <8 || charLength > 128 || isNaN(charLength)) {
+  if (charLength <8 || charLength > 128 || isNaN(charLength)) {
     alert("Start again. Please enter a number between 8 and 128 for your password length.");
     return "";
+
+  }
+
+/* Prompt user for Password Character preferences with if conditionall statements which will then concatenate
+   the users choice to chosenChar string and store it for the generating password process. */
+
+  if (confirm("Do you want to include lowercase letters in your password? Please select OK for YES or CANCEL for NO.")) {
+    chosenChar += lowcaseChar;
+
+  }
+
+  if (confirm("Do you want to include uppercase letters in your password? Please select OK for YES or CANCEL for NO.")) {
+    chosenChar += uppercaseChar;
+    
+  }
+
+  if (confirm("Do you want to include numbers in your password? Please select OK for YES or CANCEL for NO.")) {
+    chosenChar += numericChar;
+    
+  }
+
+  if (confirm("Do you want to include special characters (symbols) in your password? Please select OK for YES or CANCEL for NO.")) {
+    chosenChar += specialChar;
+  
+  }
+
+// If condition and alert function to check if variable chosenChar is empty, or user selected characters to generate the password.
+
+  if (chosenChar === "") {
+    alert("You must select a character type to generate a password. Please start again.");
+    return "";
+    
   }
 
 
 
 
-  
+
 }  
