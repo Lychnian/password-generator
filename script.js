5765a8fe1402ad7a285f93aff54356e261dc29c1
unknown <helenvcolon@gmail.com>
2023-10-12 22:52:11 -0400

    edited beginning code and variables, added first prompt, number length if statement.

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
-/* // Assignment Code
+// Assignment Code for Password Generator.
+
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// Function to write and select password to the #password input.
+
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-*/
-
+ 
 // Variables for Password
 
-var lowcasChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var uppercasChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var numericChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"];
+  var lowcaseChar = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numericChar = "0123456789";
+  var specialChar = "!@#$%^&*()-_=+";
 
 // Prompt user to choose if they want lower case, upper case, numeric, and/or special characters in their password.
 
-var charLength = prompt (
-  "How many characters do you want in your password? Choose a length (number) between 8 to 128.)"
-);
+  var charLength = parseInt(prompt ("Please enter a number between 8 to 128 for your desired password length."));
+
+  If (charLength <8 || charLength > 128 || isNaN(charLength)) {
+    alert("Start again. Please enter a number between 8 and 128 for your password length.");
+    return "";
+  }
 
 
+
+
+  
+}  
